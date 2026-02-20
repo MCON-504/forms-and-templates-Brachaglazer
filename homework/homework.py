199c45612343cc71b76fb6499170796b2d446789
Bracha Glazer <brachaglazer1@gmail.com>
2026-02-19 20:55:18 -0500

    Homework assignment

--- homework/homework.py
+++ homework/homework.py
@@ -22,14 +22,19 @@ def add_student():
         name = request.form.get("name")
         grade = request.form.get("grade")
 
-        # TODO:
-        # 1. Validate name
-        # 2. Validate grade is number
-        # 3. Validate grade range 0–100
-        # 4. Add to students list as dictionary
-        # 5. Redirect to /students
+        if not name or name.strip() == "":
+            error = "Name is required."
+            name = None
+        if not grade or not int(grade):
+            error = "Grade is required."
+            grade = None
+        if int(grade) < 0 or int(grade) > 100:
+            error = "Grade must be between 0 and 100."
+            grade = None
+        if not error:
+            students.append({"name": name, "grade": grade})
+            return redirect(url_for("display_students"))
 
-        pass
 
     return render_template("add.html", error=error)
 
@@ -50,11 +55,19 @@ def summary():
     # TODO:
     # Calculate:
     # - total students
-    # - average grade
-    # - highest grade
-    # - lowest grade
+    total_students = len(students)
+    total_grades = 0
+    max_grade = None
+    min_grade = None
+    if total_students > 0:
+        for student in students:
+            total_grades += int(student["grade"])
+            if max_grade is None or int(student["grade"]) > max_grade:
+                max_grade = int(student["grade"])
+            if min_grade is None or int(student["grade"]) < min_grade:
+                min_grade = int(student["grade"])
 
-    return render_template("summary.html")
+    return render_template("summary.html", total_students=total_students, total_grades=total_grades, min_grade=min_grade, max_grade=max_grade)
 
 
 if __name__ == "__main__":
